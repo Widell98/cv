cecede1ab888bcfd90d37dba0745ff608efa9435
widell98 <erik.widell@hotmail.com>
2023-01-23 21:21:32 +0100

    laptop

--- src/App.js
+++ src/App.js
@@ -1,66 +1,12 @@
-import twitter from './images/twitter.png'
-import logo from './images/curriculum.png'
-import linkedin from './images/linkedin.png'
-import github from './images/github.png'
-
-
+import Header from './components/Header'
+import Information from './components/Information'
 
 function App() {
   return (
     <div className="App">
-
-      <section class="hero">
-        <nav>
-          <img
-            class="logo"
-            alt="Erik Widell´s logga"
-            src={logo}
-          />
-          <ul>
-          
-            <li>About me</li>
-            <li>Projects</li>
-            <li>Contacs</li>
-          </ul>
-          <button class="hamburger" id="hamburger">
-          <i class="fas fa-bars"> </i>  
-          </button>
-        </nav>
-        <div class="hero-area">
-          <div class="hero-text">
-            <h1> Erik Widell</h1>
-            <p>Full Stack Developer</p>
-            <div class="button">About me</div>
-          </div>
-          <div class="socials">
-            <div>
-              <a href="https://www.twitter.com" class="social">
-                <img src={twitter} class="twitter" />
-              </a>
-            </div>
-            <div>
-              <a href="https://www.linkedin.com" class="social">
-                <img src={linkedin} class="linkedin" />
-              </a>
-            </div>
-            <div>
-              <a href="https://www.github.com" class="social">
-                <img src={github} class="github" />
-              </a>
-            </div>
-          </div>
-        </div>
-
-      </section>
-      <section class="sub-section">
-        <div class="information">
-          <h2>About me</h2>
-          <p> Hello. My name is Erik Widell. I am a Software Developer etc..</p>
-        </div>
-        <div class="headshot-container">
-          <img class="headshot" src="" />
-        </div>
-      </section>
+      <Header />
+      <Information />
+      <br/>
       <section class="sub-section-alternative">
         <h2>Projects</h2>
         <div class="project-container">
